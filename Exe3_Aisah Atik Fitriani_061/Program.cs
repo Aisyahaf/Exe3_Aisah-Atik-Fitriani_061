0899cd4a76f898415f4b20b5f21c014a5f991e0b
Aisah Fitriani <aisah.atik.ft21@mail.umy.ac.id>
2022-11-23 15:45:26 +0700

    Add Method public bool Search

--- Exe3_Aisah Atik Fitriani_061/Program.cs
+++ Exe3_Aisah Atik Fitriani_061/Program.cs
@@ -23,6 +23,21 @@ namespace Exe3_Aisah_Atik_Fitriani_061
             LAST = null;
         }
 
+        public bool Search(int rollNo, ref Node previous, ref Node current)
+        {
+            for (previous = current = LAST.next;
+                current != LAST;
+                previous = current,
+                current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                    return (true);
 
+            }
+            if (rollNo == LAST.rollNumber)
+                return true;
+            else
+                return (false);
+        }
     }
 }
