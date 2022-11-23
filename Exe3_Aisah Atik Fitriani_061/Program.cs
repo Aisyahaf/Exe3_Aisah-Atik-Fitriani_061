1031f884de4c028ea9ba7ff9865e717967d09ff9
Aisah Fitriani <aisah.atik.ft21@mail.umy.ac.id>
2022-11-23 15:34:48 +0700

    Add project files.

---
+++ Exe3_Aisah Atik Fitriani_061/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe3_Aisah_Atik_Fitriani_061
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
